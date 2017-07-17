a3f9fd034f3780c997203e484ee15e529b23229d
Patrick White <byelipk@kso2.local>
2017-07-17 16:31:55 -0400

    Only run fabrication initializer in dev or test

--- config/initializers/fabrication.rb
+++ config/initializers/fabrication.rb
@@ -1,6 +1,8 @@
-Fabrication.configure do |config|
-  # config.fabricator_path = 'data/fabricators'
-  # config.path_prefix = Rails.root
-  # config.sequence_start = 10000
-  # config.generators << CustomGeneratorForORM
+unless Rails.env.production?
+  Fabrication.configure do |config|
+    # config.fabricator_path = 'data/fabricators'
+    # config.path_prefix = Rails.root
+    # config.sequence_start = 10000
+    # config.generators << CustomGeneratorForORM
+  end
 end
